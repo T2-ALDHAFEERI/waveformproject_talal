03d1b373c3cec10e62064ec9d664d75cecc6e9a9
Talal Aldhafeeri <Talal2.Aldhafeeri@live.uwe.ac.uk>
2026-04-15 18:42:06 +0100

    Add waveformsample struct to waveform.h

--- waveform.h
+++ waveform.h
@@ -4,5 +4,16 @@
 
 #ifndef TALALPOWERWAVE_PROJECT_WAVEFORM_H
 #define TALALPOWERWAVE_PROJECT_WAVEFORM_H
+struct  waveformsample
+{
+    double time_stamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double the_percent;
 
+};
 #endif //TALALPOWERWAVE_PROJECT_WAVEFORM_H
